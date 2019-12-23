975d3bb91f20a6d36a42ba7e31726d447b137e4d
Nikoto <nikotomad@gmail.com>
2019-12-24 00:30:38 +0100

    Minor changes to app + consuming event routes

--- server/app.js
+++ server/app.js
@@ -6,13 +6,18 @@ const app = express();
 const PORT = process.env.PORT;
 
 const baseRoutes = require('./routes/base');
+const eventRoutes = require('./routes/event');
+
+require('./configs/database');
+
 app.use('/', baseRoutes);
+app.use('/event', eventRoutes);
 
 app.listen(PORT, (error) => {
     if (error) {
-      return console.log('Trouble starting the server:', error)
+      return console.log('Trouble starting the server:', error);
     }
-    console.log(`Listening on ${PORT}`)
+    console.log(`# Listening on ${PORT} #`)
 });
 
 module.exports = app;
