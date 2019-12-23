7c7a15c9897d0798c47590fbe46b074b39b837b3
Nikoto <nikotomad@gmail.com>
2019-12-24 00:29:34 +0100

    single quotes

--- server/routes/base.js
+++ server/routes/base.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 router.get('/', (request, response) => {
     response.status(200).json({
-        message: "✅ Health check ✅"
+        message: '✅ Health check ✅'
     });
 });
 
 router.get('/events', (req, res) => {
     console.log('Events');
     return res.status(200).json({
-        "events": "1"
+        'events': '1'
     })
 })
 
