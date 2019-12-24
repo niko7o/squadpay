67b97d2cb3dfd464a6eafd2ce78e0eacf9745619
Nikoto <nikotomad@gmail.com>
2019-12-24 19:50:33 +0100

    Created Payment model

---
+++ server/models/Payment.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const PaymentSchema = new mongoose.Schema({
+  from: {
+    type: ObjectId
+  },
+  to: {
+    type: ObjectId
+  },
+  amount: {
+    type: Number,
+    required: 'Amount is required',
+    min: 1
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  paid: {
+    type: Boolean,
+    default: false
+  },
+}, {
+  timestamps: true
+});
+
+const Event = mongoose.model('Event', PaymentSchema);
+
+module.exports = Event;
