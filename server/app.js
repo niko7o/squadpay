aed56c2f8a56d917dd2429685f50e023913200f4
Nikoto <nikotomad@gmail.com>
2020-02-20 23:14:15 +0100

    cleanup of the structure

--- server/app.js
+++ server/app.js
@@ -1,23 +1,23 @@
 /* eslint-disable no-undef */
-require('dotenv').config({ debug: process.env.DEBUG });
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const baseRoutes = require('./routes/base');
+const rootRoutes = require('./routes/root');
 const eventRoutes = require('./routes/event');
 
 require('./configs/database');
 
-app.use('/', baseRoutes);
-app.use('/event', eventRoutes);
+app.use('/', rootRoutes);
+app.use('/events', eventRoutes);
 
 app.listen(PORT, (error) => {
     if (error) {
       return console.log('Trouble starting the server:', error);
     }
-    console.log(`# SquadPay Server Listening on PORT ${PORT}`)
+    console.log(`Squadpay server listening on PORT ${PORT}`)
 });
 
 module.exports = app;
