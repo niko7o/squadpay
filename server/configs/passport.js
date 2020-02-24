4068ec750f4564dda07d90bb5e9318099aa5f938
Nikoto <nikotomad@gmail.com>
2020-02-24 20:43:10 +0100

    new passport config for local-auth and util fns

--- server/configs/passport.js
+++ server/configs/passport.js
@@ -1,24 +1,84 @@
 const User = require('../models/User');
 
 const passport = require('passport');
-const GithubStrategy = require('passport-github').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+const GitHubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
+passport.serializeUser((user, next) => {
+  next(null, user._id);
 });
 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
+passport.deserializeUser((id, next) => {
+  User.findById(id)
+    .then(user => {
+      next(null, user);
+    })
+    .catch(error => {
+      next(error);
+    });
 });
+
+passport.use('local-auth', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+}, (email, password, next) => {
+  User.findOne({ 
+    email: email, 
+    // active: true @TO-DO: For the future in case we want to disable users from logging in squadpay
+  }).then(user => {
+      if (user) {
+        return user.checkPassword(password)
+          .then(match => {
+            if (match) {
+              next(null, user);
+            } else {
+              next(null, null, { password: 'Invalid email or password' })
+            }
+          });
+      } else {
+        next(null, null, { password: 'Invalid email or password' })
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+}));
 
 passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: '/return'
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ githubId: profile.id }, function (err, user) {
-      return cb(err, user);
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+}, function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile);
     });
   }
 ));
+
+function generateRandomPass() {
+  return Math.random().toString(36);
+};
+
+function authenticateOAuthUser(accessToken, refreshToken, profile, next) {
+  let socialId = `${profile.provider}Id`;
+  User.findOne({ [`social.${socialId}`]: profile.id })
+    .then(user => {
+      if (user) {
+        next(null, user);
+      } else {
+        user = new User({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          password: generateRandomPass(),
+          social: {
+            [socialId]: profile.id
+          }
+        })
+        return user.save()
+          .then(user => {
+            next(null, user);
+          });
+      }
+    }).catch(error => next(error));
+}
