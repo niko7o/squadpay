2df00c772b0294de81e5072594e582b521f9230d
Nikoto <nikotomad@gmail.com>
2020-02-24 20:42:08 +0100

    basic app.js cleaning for structure

--- server/app.js
+++ server/app.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const app = express();
+const passport = require('passport');
 const PORT = process.env.PORT || 5000;
+const app = express();
 
 const rootRoutes = require('./routes/root');
 const eventRoutes = require('./routes/event');
+const userRoutes = require('./routes/user');
 
 require('./configs/database');
 require('./configs/passport');
@@ -15,21 +16,19 @@ require('./configs/passport');
 // Session, cookie, passport handling
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(require('cookie-parser')());
 app.use(require('express-session')({ 
   secret: process.env.EXPRESS_SESSION_SECRET, 
   resave: true, 
   saveUninitialized: true
 }));
 
-app.use(require('cookie-parser')());
-
-
 // req.body parser for POST requests
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
 app.use('/', rootRoutes);
+app.use('/users', userRoutes);
 app.use('/events', eventRoutes);
 
 app.listen(PORT, (error) => {
